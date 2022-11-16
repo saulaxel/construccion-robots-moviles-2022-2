a52b6d744c3943cc42a6de62f0fe6091f847f14b
Saul Axel <saulaxel.50@gmail.com>
2022-11-16 15:04:27 -0600

    Lectura del ultrasónico

--- maquina_estados/maquina_estados.ino
+++ maquina_estados/maquina_estados.ino
@@ -7,6 +7,9 @@
  *                                                                      *
  ************************************************************************/
 
+// Macros
+#define TAM_ARRAY(arr) (sizeof(arr) / sizeof(*(arr)))
+
 // Definicion de constantes
 #define ADELANTE move_robot(AVANCE, 0.0f)
 #define ATRAS move_robot(-AVANCE, 0.0)
@@ -15,10 +18,11 @@
 #define ALTO move_robot(0.0,0.0)
 
 enum motors {
-  up_left,
-  up_right,
-  down_left,
-  down_right  
+  up,
+  right,
+  down,
+  left,
+  num_motors // Cantidad de motores
 };
 namespace pin {
   const int motor_enable[] = { 8, 9, 10, 11 };
@@ -32,12 +36,28 @@ namespace pin {
   const int ultra_echo[] = { 23, 25, 27 };
 }
 
+// La cantidad que hay de cada tipo de sensor
+const size_t num_ldrs = TAM_ARRAY(pin::ldr);
+const size_t num_infras = TAM_ARRAY(pin::infra);
+const size_t num_ultras = TAM_ARRAY(pin::ultra_trig);
+
+// Auxiliares para el ultrasónico
+const float centimetros_por_microsegundo = (1.0f / 29.2f);
+const long umbral_ultra = 15;
+
+// Auxiliares para el motor
+enum Direction {
+  clockwise,
+  anticlockwise,
+};
+int motor_speed = 50;
+
 // Configuración de los pines de entrada y salida
 void setup() {
   Serial.begin(9600); // Inicializa comunicación serial
   using namespace pin;
   //Configuración de los motores
-  for (int i = 0; i < 4; ++i) {
+  for (size_t i = 0; i < num_motors; ++i) {
     pinMode(motor_enable[i], OUTPUT);
     pinMode(motor_direction[i], OUTPUT);
     pinMode(encoder_outA[i], INPUT);
@@ -46,36 +66,84 @@ void setup() {
 
 
   //Configuración de los sensores de entrada
-  for (int i = 0; i < 4; ++i) {
-    pinMode(ldr[i], INPUT);
+  for (auto pin_ldr : pin::ldr) {
+    pinMode(pin_ldr, INPUT);
   }
-  for (int i = 0; i < 4; ++i) {
-    pinMode(infra[i], INPUT);
+
+  for (auto pin_infra : pin::infra) {
+    pinMode(pin_infra, INPUT);
   }
-  for (int i = 0; i < 4; ++i) {
+
+  for (size_t i = 0; i < num_ultras; ++i) {
     pinMode(ultra_trig[i], OUTPUT);
-  }
-  for (int i = 0; i < 4; ++i) {
     pinMode(ultra_echo[i], INPUT);
   }
 }
+
+void setMotorDirection(int index, Direction d) {
+  if (d == clockwise) {
+    digitalWrite(pin::motor_direction[index], HIGH);
+    //digitalWrite(pin::motor_direction[index], LOW);
+  } else {
+    //digitalWrite(dirPin1, LOW);
+    //digitalWrite(dirPin2, HIGH);
+  }
+}
+
+void setMotorSpeed(int motor, int motorSpeed) {
+  //analogWrite(speedPin, motorSpeed);
+}
+
+void avance_robot(float distancia)
+{
+  
+}
+
+void giro_robot(float angulo)
+{
+  
+}
+
 // Esta función hace que el robot primero gire un angulo y después avance una distancia
 void move_robot(float distancia, float angulo)
 {
-  // ....
+  giro_robot(angulo);
+  avance_robot(distancia);
   delay(1000);
 }
 // Esta función lee el valor de un sensor, indicando su tipo y numero, y regresa su valor
-float shs(char *sensor, int num_sensor)
+float shs(String sensor, int num_sensor)
 {
   float x=1.0;
-  // ....
+
+  if (sensor == "ultra") {
+    long tiempo_ir_regresar_us, tiempo_ida_us;
+    long distancia_cm;
+    
+    // Indicar al sensor ultrasónico que envíe una onda de sonido
+    digitalWrite(pin::ultra_trig[num_sensor], LOW);
+    delayMicroseconds(4);
+    digitalWrite(pin::ultra_trig[num_sensor], HIGH);
+    delayMicroseconds(10);
+    digitalWrite(pin::ultra_trig[num_sensor], LOW);  
+
+    // Leemos de Echo el tiempo que tarda la onda en regresar
+    tiempo_ir_regresar_us = pulseIn(pin::ultra_echo[num_sensor], HIGH);
+    tiempo_ida_us = tiempo_ir_regresar_us / 2;
+
+    distancia_cm = tiempo_ida_us * centimetros_por_microsegundo;
+
+    if (distancia_cm < umbral_ultra) {
+      x = 0;
+    }
+  }
+  
   return(x);
 }
 // Algoritmo de evasión de obstáculos
 void loop(){
   int estado = 0;
-  float Si,Sd;
+  float Si, Sm, Sd;
   float AVANCE=.1;
   float GIRO= 0.7854;
   // Estado inicial
@@ -83,8 +151,10 @@ void loop(){
   // Loop infinito
   while(1){
     // Se leen los sensores
-    Si = shs("contact",1);
-    Sd = shs("contact",2);
+    Si = shs("ultra", 0);
+    Sm = shs("ultra", 1);
+    Sd = shs("contact", 2);
+    
     Serial.print("Estado Presente: ");
     Serial.println(estado);
     Serial.print("Si: ");
