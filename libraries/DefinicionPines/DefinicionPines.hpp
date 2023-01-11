010702064dce84666a4c07fb2553ba3ac3edb3e3
Saul Axel <saulaxel.50@gmail.com>
2023-01-11 17:55:57 -0600

    Renombrar pines

--- libraries/DefinicionPines/DefinicionPines.hpp
+++ libraries/DefinicionPines/DefinicionPines.hpp
@@ -3,8 +3,8 @@
 namespace pin {
   const int motor_enable[] = { 7, 8, 9, 10 };
   const int motor_direction[] = { 40, 41, 42, 43 };
-  const int encoder_outA[] = { 30, 32, 34, 36 };
-  const int encoder_outB[] = { 31, 33, 35, 37 };
+  const int encoder_A[] = { 31, 33, 35, 37 };
+  const int encoder_B[] = { 30, 32, 34, 36 };
 
   const int ldr[] = { A0, A1, A2, A3 };
   const int infra[] = { A4, A5, A6, A7 };
